b9d6b72814dee07ce3bd8f5abec1bdaa908f07f6
dilip0509 <38337224+dilip0509@users.noreply.github.com>
2025-04-30 07:49:49 +0530

    clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,8 +1,6 @@
-def call(string url, string branch)
-{
-  git url:${url},branch:${branch}
-  echo "Code clone successfull"
+def call(String url, String branch) {
+    git url: url, branch: branch
+    echo "Code clone successful"
 }
-
 
                 
