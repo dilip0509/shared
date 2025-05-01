eb7977eeb90207933b437a514e53c7dd90719ca5
dilip0509 <38337224+dilip0509@users.noreply.github.com>
2025-05-01 06:10:25 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
 def call(String image_name,String version, String user_name)
 {
-  sh "docker build -t user_name/image_name:version ."
+  sh "docker build -t ${user_name}/${image_name}:${version} ."
 }
